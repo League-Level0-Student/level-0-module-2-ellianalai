809f3506f55ece53cb13813be05b572478efcb13
league <league@imac-34.attlocal.net>
2023-05-12 16:31:03 -0700

    aetnjrtgnsehtnb

--- src/_03_string_conversion/_4_voting_booth/voting_booth.java
+++ src/_03_string_conversion/_4_voting_booth/voting_booth.java
@@ -1,10 +1,22 @@
 package _03_string_conversion._4_voting_booth;
 
+import javax.swing.JOptionPane;
+
 public class voting_booth {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String age = JOptionPane.showInputDialog("How old are you?");
+		int ageInt = Integer.parseInt(age);
+		if (ageInt>=18) {
+			JOptionPane.showMessageDialog(null, "Who do you think the next president should be?");
+		}
+		else if (ageInt<18) {
+			JOptionPane.showMessageDialog(null, "Nobody cares what you think");
+		}
+		
+	
+	
+	
 	}
 
 }
